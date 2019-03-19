217b8f7121c9d980eb5343e612f741b2d4f30f84
Neelaesh <neelaesh006@gmail.com>
2019-03-19 23:06:01 +0530

    Angular Express

--- BookStore_Express_Mongo/Configurations/config.js
+++
@@ -1,7 +0,0 @@
-var mongourl = "mongodb://localhost:27017";
-var dbname = "bookStore";
-var port = 3000;
-
-module.exports.mongourl = mongourl;
-module.exports.dbname = dbname;
-module.exports.port = port;
--- BookStore_Express_Mongo/env-var.js
+++
@@ -1,2 +0,0 @@
-process.env.DB_URL = 'mongodb://localhost:27017';
-process.env.DB_NAME = 'bookStore';
